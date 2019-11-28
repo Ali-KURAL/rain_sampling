6e4bfebdd84bec13f93ff4468cf36a662ea51399
Mehmet Kaan ERKOÇ <mkaanerkoc@gmail.com>
2019-11-28 17:58:43 +0100

    initialState parameter added on startReading

--- Core/Inc/EventGenerator.h
+++ Core/Inc/EventGenerator.h
@@ -37,7 +37,7 @@ EventSystemHandler_t EventGenerator_AddInput(
 );
 
 void EventGenerator_ReadInputs( uint8_t readPeriod );
-EventGenerator_Result EventGenerator_StartReading( EventSystemHandler_t arg );
+EventGenerator_Result EventGenerator_StartReading( EventSystemHandler_t arg, uint8_t initialState);
 EventGenerator_Result EventGenerator_StopReading( EventSystemHandler_t arg );
 
 
